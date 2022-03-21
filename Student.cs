c83458b62bb883b278c7082d47ac217766d7ac07
Maliha-Zaman <95135458+Maliha-Zaman@users.noreply.github.com>
2022-03-21 22:43:02 +0600

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS
+{
+    internal class Student
+    {
+        public int id;
+        public string name;
+        public string department;
+        public int semester;
+
+        public string getInfo()
+        {
+            string studentInfo = id.ToString() + "\t" + name + "\t" + department + semester.ToString();
+            return studentInfo;
+        }
+    }
+
+}
---
+++ teachers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS
+{
+    internal class teachers
+    {
+        public int ID;
+        public string Name;
+        public  string dept;
+        public  string designation;
+        public int salary;
+
+        public string getInfo()
+        {
+            string teacherInfo  = ID.ToString() + "\t" + Name + "\t" + dept +"\t"+ designation + "\t" + salary.ToString();
+            return teacherInfo;
+        }
+
+    }
+}
